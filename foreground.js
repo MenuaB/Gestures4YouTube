c0e51e3c55a8becbb913e49aac992aac549e3b70
menuab <menooa.vold@gmail.com>
2021-09-20 17:50:31 +0400

    added images and ai files

---
+++ foreground.js
@@ -0,0 +1,6 @@
+// document.querySelector("#player").style.transform = 'rotate(100deg)';
+// var player = document.querySelector("#player");
+// player.ariaMultiSelectable()
+document.querySelector('video').currentTime = 3000;
+document.querySelector('video').pause();
+// document.querySelector('.ytp-button-next').click()
